1756325c9c6613205764a21cd2d88fdd5db47f87
Santiago Navarro Ruiz <santiacho@gmail.com>
2017-03-22 22:22:11 +0100

    [GE]: First commit.

---
+++ Gioseppo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gioseppo
+//
+//  Created by Santiago Navarro Ruiz on 22/3/17.
+//  Copyright © 2017 Santiago Navarro Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
